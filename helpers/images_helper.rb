5f9acf77754757a9a198405b02d7bccd7e15e272
Gabriele Canepa <gabriele@canepa.io>
2019-05-15 21:38:53 -0800

    Update images helper

--- helpers/images_helper.rb
+++ helpers/images_helper.rb
@@ -1,13 +1,13 @@
 module ImagesHelper
   def svg_tag(file_name, attributes = {})
-    root      = Middleman::Application.root
-    file_path = File.join(root, 'source', config[:images_dir], file_name)
-    svg       = File.exist?(file_path) ? File.read(file_path) : 'SVG not found'
-    name      = file_name.gsub('.svg', '')
+    root       = Middleman::Application.root
+    file_path  = File.join(root, 'source', config.images_dir, file_name)
+    svg        = File.exist?(file_path) ? File.read(file_path) : 'SVG not found'
+    image_name = file_name.gsub('.svg', '')
 
-    svg.gsub!('<svg ', new_opening_tag(attributes))
+    add_attributes_to_opening_tag(svg, 'svg', attributes)
 
-    (0..99).each { |n| change_class_name(svg, "st#{n}", "#{name}#{n}") }
+    (0..99).each { |n| change_class_name(svg, "st#{n}", "#{image_name}#{n}") }
     svg.slice!(/\b(?:height|width)\b/)
 
     svg
@@ -15,12 +15,13 @@ module ImagesHelper
 
   private
 
-  def new_opening_tag(attributes)
-    svg_opening_tag = '<svg '
+  def add_attributes_to_opening_tag(element, tag, attributes)
+    opening_tag     = "<#{tag} "
+    new_opening_tag = opening_tag
     attributes.each_key do |attribute|
-      svg_opening_tag += "#{attribute}=\"#{attributes[attribute]}\" "
+      new_opening_tag += "#{attribute}=\"#{attributes[attribute]}\" "
     end
-    svg_opening_tag
+    element.gsub!(opening_tag, new_opening_tag)
   end
 
   def change_class_name(element, old_name, new_name)
